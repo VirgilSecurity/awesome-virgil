56901af2ba25c077edd63221703a4c74633054e4
diablero13 <diablero13@jsdev.local>
2016-12-21 12:22:57 +0200

    [WWW-429] Changes in builder configuration which related to new theme structure

--- docs/conf.py
+++ docs/conf.py
@@ -1,99 +1,80 @@
-# -- General configuration ------------------------------------------------
+#-------------------------------------------------------------------------
+#------------------- General configuration -------------------------------
+#-------------------------------------------------------------------------
 
-# General information about the project.
-project = u'Virgil Security Docs'
+project   = u'Virgil Security Documentation'
 copyright = u'2016, Virgil Security, Inc'
-author = u'Virgil Security, Inc.'
+author    = u'Virgil Security, Inc.'
 
-extensions = [
-    'sphinx.ext.viewcode',
-#    'sphinxcontrib.httpdomain',
-#    'sphinxcontrib.jsoncall'
+source_suffix = '.rst'
+master_doc    = 'index'
+
+templates_path = [
+    'pages',
+    'templates'
 ]
 
-templates_path = [ 'pages' ]
-source_suffix = '.rst'
-master_doc = 'index'
 
 exclude_patterns = [
-    'cli/v3*'
+     'cli/v3*'
 ]
 
+#-------------------------------------------------------------------------
+#------------------- Extensions configuration ----------------------------
+#-------------------------------------------------------------------------
 
-# -- Options for HTML output ----------------------------------------------
+extensions = [
+#   'sphinx.ext.viewcode',
+#   'sphinxcontrib.httpdomain',
+#   'sphinxcontrib.jsoncall'
+]
 
+# jsoncall_baseurl = 'https://identity.virgilsecurity.com/v1'
 
-html_theme_path = ["./_themes"]
-html_theme = 'sphinx_virgil_theme'
+#-------------------------------------------------------------------------
+#------------------- Options for HTML output -----------------------------
+#-------------------------------------------------------------------------
+
+html_theme         = 'sphinx_virgil_theme'
 html_theme_options = {}
 
-html_title = u'Virgil Security Docs'
-html_short_title = u'Virgil Security Docs'
-html_logo = "./images/logo.png"
-html_favicon = "./images/favicon.ico"
+html_theme_path    = [
+    './_themes'
+]
 
-html_static_path = ['_static']
+html_static_path = [
+    '_static'
+]
 
-# Add any extra paths that contain custom files (such as robots.txt or .htaccess) here, relative to this directory.
-html_extra_path = []
+html_extra_path = [
+    'extra'
+]
 
-# Additional templates that should be rendered to pages, maps page names to template names.
 html_additional_pages = {
-    'index' : 'pages/custom-index.html'
+    'index'    : 'pages/custom-index.html',
 #   'sdk/sdks' : 'pages/custom-sdk.html'
 }
 
-html_copy_source = False
+html_sidebars = {
+    '**': ['searchbox.html', 'localtoc.html', 'sourcelink.html']
+#   'index': ['index_sidebar.html'],
+}
 
-html_show_sphinx = False
-html_show_copyright = True
+html_title         = u'Virgil Security Documentation'
+html_short_title   = u'Documentation'
+html_favicon       = './images/favicon.ico'
+html_logo          = './images/logo.png'
 
-# html_use_opensearch = ''
-html_search_language = 'en'
+html_copy_source       = True
+html_domain_indices    = True
+html_split_index       = False
+html_show_sourcelink   = True
+html_add_permalinks    = False
 
+html_scaled_image_link = False
+html_use_smartypants   = False
+html_compact_lists     = True
 
-jsoncall_baseurl = 'https://identity.virgilsecurity.com/v1'
+html_show_sphinx       = False
+html_show_copyright    = False
 
-htmlhelp_basename = 'VirgilDocsdoc'
-
-
-# -- Options for manual page output ---------------------------------------
-
-# One entry per manual page. List of tuples (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'virgildocs', u'Virgil Security Documentation', [author], 1)
-]
-
-man_show_urls = False
-
-
-# -- Options for Texinfo output -------------------------------------------
-
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (master_doc, 'VirgilDocs', u'Virgil Security Documentation',
-     author, 'VirgilDocs', 'One line description of project.',
-     'Miscellaneous'),
-]
-
-# Documents to append as an appendix to all manuals.
-#
-# texinfo_appendices = []
-
-# If false, no module index is generated.
-#
-# texinfo_domain_indices = True
-
-# How to display URL addresses: 'footnote', 'no', or 'inline'.
-#
-# texinfo_show_urls = 'footnote'
-
-# If true, do not generate a @detailmenu in the "Top" node's menu.
-#
-# texinfo_no_detailmenu = False
-
-
-# Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
